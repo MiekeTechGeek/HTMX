31615cd5e89083184be3e95a12cc7459192bcdad
Mieke Pienaar <miekepienaar435@gmail.com>
2025-03-28 10:09:21 +0200

    first commit

--- tut/application1/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class SampleForm(forms.Form):
-    name = forms.CharField(min_length=3, max_length=100)
-    email = forms.EmailField()
-    favourite_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color'})) 
